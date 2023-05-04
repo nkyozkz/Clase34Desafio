6df37c025328ac517e0b57c7523e4b9db675f73d
nkyozkz <nicolas.unizkz@gmail.com>
2023-05-04 19:03:59 -0400

    Subida de archivos

---
+++ src/routes/routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import cartsRouter from "../components/carts/routes.js";
+import chatRouter from "../components/chats/routes.js";
+import productsRouter from "../components/products/routes.js";
+import sessionRouter from "../components/users/routes.js";
+import ticketRouter from "../components/ticket/routes.js";
+import mockingProducts from "../components/mock/routes.js";
+import loggerTest from "../components/log/routes.js";
+import handlebarsRouter from "../client/handlebarRoutes.js";
+
+const router = Router();
+
+router.use("/", handlebarsRouter);
+router.use("/loggerTest", loggerTest);
+router.use("/mockingproducts", mockingProducts);
+router.use("/api/ticket", ticketRouter);
+router.use("/api/sessions", sessionRouter);
+router.use("/api/carts", cartsRouter);
+router.use("/api/chat", chatRouter);
+router.use("/api/products", productsRouter);
+
+export default router;
